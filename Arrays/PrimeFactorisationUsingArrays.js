a5f240abaeaf6e1ec4c705774eed68fb6d918185
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-04-21 09:13:01 +0530

    Added code for Arrays and Maps

---
+++ Arrays/PrimeFactorisationUsingArrays.js
@@ -0,0 +1,23 @@
+var userInput = require("readline-sync");
+
+function primeFactors(number) {
+    const factors = [];
+    for (i = 2; i <= userInputNum; i++) {
+        if (userInputNum % i == 0) {
+            var flag = 0;
+            for (j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                factors.push(i);
+            }
+        }
+    }
+    return factors
+}
+
+userInputNum = parseInt(question("Enter number: "));
+console.log("Prime factors for " + userInputNum + " are: " + primeFactors(userInputNum));
